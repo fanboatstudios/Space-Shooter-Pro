233b778adcb84d3047183f31455c4105f5710e2f
Charles Pence <charles@fanboatstudios.com>
2020-01-14 02:04:29 -0600

    Player Movement Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float PlayerSpeed = 1f;
+    private float PlayerBoundsX = 11.27f;
+    private float PlayerBoundsUpperY = 0f;
+    private float PlayerBoundsLowerY = -3.8f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TranslateMovement();
+    }
+
+    private void TranslateMovement()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        if (Mathf.Abs(transform.position.x) > PlayerBoundsX)
+        {
+            transform.position = new Vector3(-1 * transform.position.x, transform.position.y, 0);
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, PlayerBoundsLowerY, PlayerBoundsUpperY), 0);
+
+        transform.Translate(direction * Time.deltaTime * PlayerSpeed);
     }
 }
